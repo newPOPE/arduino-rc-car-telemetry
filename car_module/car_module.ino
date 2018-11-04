c15ca26f9193f8087cf0ffb797f32a3f455d1682
Adam Stipak <adam.stipak@gmail.com>
2018-11-04 11:10:49 +0100

    Switch to MPU6050_tockn library.

--- car_module/car_module.ino
+++ car_module/car_module.ino
@@ -1,7 +1,7 @@
-#include <I2Cdev.h>
-#include <MPU6050_6Axis_MotionApps20.h>
 #include <SPI.h>
 #include "RF24.h"
+#include <MPU6050_tockn.h>
+#include <Wire.h>
 
 
 // RF24 init
@@ -13,86 +13,16 @@ byte recieverAddress[] = "reciever00";
 RF24 nRF(CE, CS);
 
 // mpu initialization
-MPU6050 mpu;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
+MPU6050 mpu(Wire);
 
 void setup() {
   Serial.begin(9600);
-
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
-  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
 
   Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(40);
-  mpu.setYGyroOffset(-36);
-  mpu.setZGyroOffset(-8);
-  mpu.setZAccelOffset(990); // 1688 factory default for my test chip
-
-  if (devStatus == 0) {
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(0, dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
+  mpu.begin();
+  //mpu.calcGyroOffsets(true);
+  mpu.setGyroOffsets(-1.09, 1.19, 0.17);
 
   Serial.println("Transmitter setup.");
   nRF.begin();
@@ -104,48 +34,23 @@ void setup() {
 }
 
 void loop() {
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
+  // measure angles
+  mpu.update();
 
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
+  // sending data
+  int values[2];
+  values[0] = mpu.getAngleX();
+  values[1] =  mpu.getAngleY();
 
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-    // sending data
-    int values[2];
-    values[0] = int(ypr[2] * 180 / M_PI);
-    values[1] =  int(abs(ypr[1] * 180 / M_PI));
-
-    Serial.println("Sending data");
-    nRF.stopListening();
-    if (!nRF.write(&values, sizeof(values))) {
-      Serial.println("Data not sent!");
-    }
-    else {
-      Serial.println("Data sent!");
-    }
-
-    delay(50);
+  Serial.println("Sending data");
+  nRF.stopListening();
+  if (!nRF.write(&values, sizeof(values))) {
+    Serial.println("Data not sent!");
   }
+  else {
+    Serial.println("Data sent!");
+  }
+
+  delay(50);
+
 }
